58067a10dfbd55a725f79ca0500e523dbf9c4e49
Alexander Zeitler <alexander.zeitler@pdmlab.com>
2016-04-09 13:38:27 +0200

    Cleanup

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
     res.status(200).send('hello world');
 })
 
-app.listen(3000)
+app.listen(3000);
